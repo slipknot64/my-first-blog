e02643fcd96a7caa48ced7c7dc86c969107bec8f
adil mahmood <adilgreet@gmail.com>
2021-01-31 19:22:25 +0000

    fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,18 +129,13 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+  os.path.join(BASE_DIR, 'static'),
 )
 
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL= "/"
