bfcd621f3a0d748bd921b3a8d99bbf223a3016df
adil mahmood <adilgreet@gmail.com>
2021-01-31 17:37:04 +0000

    removed STATIC_ROOT = os.path.join(BASE_DIR, 'static')

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,7 +136,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
