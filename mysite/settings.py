0d047a8bca1a1cab0a7a17829b059e02a0cdcc04
adil mahmood <adilgreet@gmail.com>
2021-01-31 19:50:37 +0000

    uninstalled fontawesome

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,6 +136,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 LOGIN_REDIRECT_URL= "/"
 
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
