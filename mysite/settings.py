3d5b1c8d14ce49448c147bf25bbae2f7f347402b
adil mahmood <adilgreet@gmail.com>
2021-01-31 22:00:30 +0000

    added env

--- mysite/settings.py
+++ mysite/settings.py
@@ -141,7 +141,7 @@ LOGIN_REDIRECT_URL= "/"
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = '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' # this is exactly the value 'apikey'
+EMAIL_HOST_USER = config('API_KEY') # this is exactly the value 'apikey'
 EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
