7333cab14afb6572ae600a9528f1edab8370b6d1
adil <adilgreet@gmail.com>
2021-04-18 18:49:22 +0100

    search bar works but needs tweaking to filter searched products

--- blog/filters.py
+++ blog/filters.py
@@ -1,1 +1,8 @@
-#create search bar in this
+import django_filters
+from .models import Product
+
+class OrderFilter(django_filters.FilterSet):
+    class Meta:
+        model = Product
+        fields = ['title']
+        filter_fields = ["field_1", "field_2"]
