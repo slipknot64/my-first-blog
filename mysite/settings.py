35b73d4afa092b9f2439a7b7b09ae40c720cd331
adil <adilgreet@gmail.com>
2021-05-11 18:57:17 +0100

    minor changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -158,6 +158,6 @@ SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER =('apikey') # this is exactly the value 'apikey'
-EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_API_KEY')
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
