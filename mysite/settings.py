e8e7bdec7f7085081fceebc0b70055dd44045d31
adil <adilgreet@gmail.com>
2021-05-19 01:22:27 +0100

    added sendgrid settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -154,11 +154,13 @@ MEDIA_ROOT =  os.path.join(BASE_DIR, 'blog/static/media')
 LOGIN_REDIRECT_URL= 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
-SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
-SENDGRID_SANDBOX_MODE_IN_DEBUG=True
+SENDGRID_API_KEY = ("SENDGRID_API_KEY")
+SENDGRID_SANDBOX_MODE_IN_DEBUG=False
 
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
-EMAIL_HOST_PASSWORD = os.environ.get("SENDGRID_API_KEY") # this is your API key
+EMAIL_HOST_PASSWORD = os.getenv("SENDGRID_API_KEY") # this is your API key
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = ("DEFAULT_FROM_EMAIL") # this is the sendgrid email
